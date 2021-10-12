d136865ff23f1221d85e1afe38e45b65ab798213
Christian Lemus <christianlemus2000@gmail.com>
2021-10-12 15:38:17 -0500

    template folder copied to custom theme + page cache disabled + basic gulp browsersync configured

---
+++ themes/custom/codelar-e-shop/gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const browserSyncReload = browserSync.reload;
+const connect = require("gulp-connect-php");
+// const sass = require("gulp-sass");
+
+gulp.task("watch", () => {
+  gulp.watch("./templates/**/*.html.twig").on("change", browserSyncReload);
+  connect.server({}, () => {
+    browserSync.init({
+      proxy: "http://localhost/codelar-e-shop/",
+    });
+  });
+});
