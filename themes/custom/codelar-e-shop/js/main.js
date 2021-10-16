068946bd21a83a26adc2b2523f1e5a7cc7d85235
Christian Lemus <christianlemus2000@gmail.com>
2021-10-16 00:16:48 -0500

    landing-page is responsive + dark theme configured

--- themes/custom/codelar-e-shop/js/main.js
+++ themes/custom/codelar-e-shop/js/main.js
@@ -4,40 +4,59 @@
     attach: () => {
       // ready to use jquery in drupal
       const html = $("html");
+      const body = $("body");
       const darkIcon = $("#dark-icon");
       const lightIcon = $("#light-icon");
       const mobileMenu = $("#mobile-menu");
       const openMenuBtn = $("#open-menu");
       const closeMenuBtn = $("#close-icon");
+      const darkMode = localStorage.getItem("darkMode");
+      var menuToggle = false;
       var toggle = false;
+      // enable theme dark
+      function enableDarkMode() {
+        toggle = true;
+        darkIcon.addClass("hidden");
+        lightIcon.removeClass("hidden");
+        html.addClass("dark");
+        body.addClass("dark:bg-bg_dark");
+        localStorage.setItem("darkMode", "enabled");
+      }
+      // disable
+      function disableDarkMode() {
+        toggle = false;
+        html.removeClass("dark");
+        darkIcon.removeClass("hidden");
+        lightIcon.addClass("hidden");
+        body.removeClass("dark:bg-bg_dark");
+        localStorage.setItem("darkMode", null);
+      }
+      // on load valid theme dark
+      if (darkMode == "enabled") {
+        enableDarkMode();
+      }
       // toggle dark -  light
-      $(".toggleDarkLight").click( function () {
+      $(".toggleDarkLight").click(function () {
         toggle = !toggle;
         if (toggle) {
-          darkIcon.addClass("hidden");
-          lightIcon.removeClass("hidden");
-          html.addClass("dark");
-        }
-        else {
-          html.removeClass("dark");
-          darkIcon.removeClass("hidden");
-          lightIcon.addClass("hidden");
+          enableDarkMode();
+        } else {
+          disableDarkMode();
         }
       });
-      // toggle mobile menu 
-      $(".toggleMenu").click( function(){
-        toggle = !toggle;
-        if (toggle) {
-          mobileMenu.addClass("hidden");
+      // toggle mobile menu
+      $(".toggleMenu").click(function () {
+        menuToggle = !menuToggle;
+        if (menuToggle) {
+          mobileMenu.removeClass("hidden");
           openMenuBtn.addClass("hidden");
           closeMenuBtn.removeClass("hidden");
         } else {
-          mobileMenu.removeClass("hidden");
+          mobileMenu.addClass("hidden");
           openMenuBtn.removeClass("hidden");
           closeMenuBtn.addClass("hidden");
         }
       });
-
     },
   };
 })(jQuery);
